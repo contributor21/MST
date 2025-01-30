e61c6ebdefe99eba2fc5f0972e895698ea0b5f76
maxph2211 <hm.pham.2023@phdcs.smu.edu.sg>
2025-01-31 02:15:01 +0800

    add

---
+++ helper.js
@@ -0,0 +1,38 @@
+function createAudioHTML(path) {
+    return '<audio controls controlslist="nodownload" class="px-1"> <source src=' +
+        path +
+        ' type="audio/wav">Your browser does not support the audio element.</audio>';
+  }
+  
+  
+  function generateExampleRow(table_row, base_path, paddedNumber, folders, col_offset) {
+    for (var i = 0; i < folders.length; i++) {
+        if (folders[i] == 'text') {
+            let p = base_path + folders[i] + '/' + paddedNumber + '.txt';
+            let cell = table_row.cells[col_offset + i];
+            var req = new XMLHttpRequest();
+            req.open("GET", p, false);
+            req.send(null);
+            cell.innerHTML = '<font size="-1">' + req.responseText + '</font>';
+        } else {
+            let p = base_path + folders[i] + '/' + paddedNumber + '.wav';
+            let cell = table_row.cells[col_offset + i];
+            cell.innerHTML = cell.innerHTML + createAudioHTML(p);
+        }
+    }
+  }
+  
+  
+  function generateT2A(tableId) {
+    let table = document.getElementById(tableId);
+    let folders = ['text', 'target', 'EchoAudio', 'teacher', 'Make-an-audio2', 'AudioLDM2', 'Tango', 'AudioLDM', 'Make-an-audio', 'AudioGen'];
+  
+    for (var i = 0; i < 20; i++) {
+      let paddedNumber = i.toString().padStart(5, '0');  
+      generateExampleRow(table.rows[1 + i], 'assets/', paddedNumber, folders, 0);
+    }
+  }
+
+
+  
+  generateT2A('supervision-efficiency-table');
